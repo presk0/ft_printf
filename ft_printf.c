47a9c6f3598108572a86e448b3f07cfcb2c0c6bc
Nicolas Dionis <nidionis@k2r1p2.42mulhouse.fr>
2024-10-23 12:04:35 +0200

    put nbr returns lenght

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nidionis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:20:59 by nidionis          #+#    #+#             */
-/*   Updated: 2024/10/22 23:48:57 by nidionis         ###   ########.fr       */
+/*   Updated: 2024/10/23 12:03:37 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,63 @@
 #define FD 1
 #define FLAGS "cspdiuxX%"
 
+int	ft_putchar(char c)
+{
+	return (write(FD, &c, 1));
+}
+
+size_t	ft_putnbr_fd(long long int n, int fd)
+{
+	static	size_t	len;
+
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else if (n < 0)
+	{
+		if (n == -9223372036854775808)
+			return (write(fd, "-9223372036854775808", 20));
+		else
+		{
+			len += write(fd, "-", 1);
+			ft_putnbr_fd(-1 * n, fd);
+		}
+	}
+	else
+	{
+		n += (int) '0';
+		len += write(fd, (char *) &n, 1);
+	}
+	return (len);
+}
+/*
 int	print_item(va_list ap, char c)
 {
 	if (c == 'c')
-		return (ft_printchar((int) va_arg(ap, char));
+		return (ft_putchar((char)va_arg(ap, int)));
 	if (c == 's')
-		return (ft_printchar((char *) va_arg(ap, char *));
+		return (ft_putstr((char *) va_arg(ap, char *)));
 	if (c == 'p')
-		return (ft_printlonghex((long int) va_arg(ap, (void *));
+		return (ft_printlonghex((long int) va_arg(ap, void *)));
 	if (c == 'd')
-		return (ft_printlongnbr((long long int) va_arg(ap, long long int));
+		return (ft_putnbr_fd((long long int) va_arg(ap, long long int), FD));
 	if (c == 'i')
-		return (ft_printnbr((int) va_arg(ap, int));
+		return (ft_printnbr((int) va_arg(ap, int)));
 	if (c == 'u')
-		return (ft_printlongnbr((unsigned int) va_arg(ap, unsigned int));
+		return (ft_putnbr_fd((unsigned int) va_arg(ap, unsigned int), FD));
 	if (c == 'x')
-		return (ft_printhex((int) va_arg(ap, int));
+		return (ft_printhex((int) va_arg(ap, int)));
 	if (c == 'X')
-		return (ft_printhexmaj((int) va_arg(ap, int));
+		return (ft_printhexmaj((int) va_arg(ap, int)));
 	if (c == '%')
 		return (write(FD, "%", 1));
 	return (1);
 }
+*/
 
+/*
 int ft_printf(const char *str, ...)
 {
    va_list	ap;
@@ -53,8 +87,8 @@ int ft_printf(const char *str, ...)
 	   return (-1);
    while (*str)
    {
-	   if (*str == '%' && ft_strchr(FLAGS, *(str))
-		   ret_val += print_item(ap, *(++str);
+	   if (*str == '%' && ft_strchr(FLAGS, *(str + 1)))
+		   ret_val += print_item(ap, *(++str));
 	   else
 		   ret_val += write(FD, str, 1);
 		str++;
@@ -63,7 +97,6 @@ int ft_printf(const char *str, ...)
    return (ret_val);
 }
 
-/*
 int va_test(const char *str, ...)
 {
    va_list ap;
@@ -80,10 +113,10 @@ int va_test(const char *str, ...)
    va_end(ap);
    return (0);
 }
-
+*/
 int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	va_test(argv[0], argv[1], argv[2]);
+	printf("\n%ld\n", ft_putnbr_fd(atoi(argv[1]), FD));
 }
